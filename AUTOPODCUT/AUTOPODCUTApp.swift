df6734a26c278f90e903778d458c6c578c0815ff
Patrik Pallagi <teaching.patrik.pallagi@gmail.com>
2025-12-24 22:53:23 +0100

    Initial Commit

---
+++ AUTOPODCUT/AUTOPODCUTApp.swift
@@ -0,0 +1,17 @@
+//
+//  AUTOPODCUTApp.swift
+//  AUTOPODCUT
+//
+//  Created by Patrik Pallagi on 2025. 12. 24..
+//
+
+import SwiftUI
+
+@main
+struct AUTOPODCUTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AUTOPODCUTTests/AUTOPODCUTTests.swift
@@ -0,0 +1,17 @@
+//
+//  AUTOPODCUTTests.swift
+//  AUTOPODCUTTests
+//
+//  Created by Patrik Pallagi on 2025. 12. 24..
+//
+
+import Testing
+@testable import AUTOPODCUT
+
+struct AUTOPODCUTTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
